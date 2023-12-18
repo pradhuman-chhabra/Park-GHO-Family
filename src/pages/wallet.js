dcb4355d035ba0be30596b99bf1f14ae31a85b2c
thecreatorpc101@gmail.com <72680645+thecreatorpc@users.noreply.github.com>
2023-12-18 14:03:42 +0530

    added new stuff in  miles wallet

--- src/pages/wallet.js
+++ src/pages/wallet.js
@@ -6,11 +6,48 @@ export default function vision() {
   return (
     <Layout>
       <Container>
-        <h1 className="section-heading">Miles Wallet</h1>
+        <h1 className="section-heading"></h1>
 
         <Col className="bot-marg" md="12" lg="12">
-          <img src="/img/wallethero.png" style={{ width: "100%" }} />
+          <img
+            src="/img/wallethero.png"
+            style={{ width: "100%", borderRadius: "20px" }}
+          />
         </Col>
+      </Container>
+
+      <Container>
+        <h1 className="section-heading">What is Miles wallet?</h1>
+
+        <Col className="bot-marg" md="12" lg="12">
+          <img
+            src="/img/featuresofwallet.png"
+            style={{ width: "100%", borderRadius: "20px" }}
+          />
+        </Col>
+        <h1 className="section-heading">
+          We’re making crypto easy and convenient to use at last
+        </h1>
+      </Container>
+
+      <Container>
+        <h1 className="section-heading">Vision</h1>
+
+        <Row>
+          <Col className="bot-marg" md="12" lg="6">
+            <img src="/img/Mask group.png" style={{ width: "100%" }} />
+          </Col>
+          <Col className="bot-marg" md="12" lg="6">
+            <h5>
+              Our vision is to use blockchain technology to make driving safer
+              and monitored for drivers, authorities, as well as other 3rd
+              parties. With a mix of hardware and software powered by Ethereum
+              blockchain services and IPFS/Filecoin decentralized storage, we
+              will provide intelligent feedback about drive quality and help
+              analyze past incidents, as well as predict future incidents
+            </h5>
+          </Col>
+        </Row>
       </Container>
     </Layout>
   );
